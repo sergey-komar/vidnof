f6354bab59400f409050268023ef73d143a56175
sergey-komar <kursev81@yandex.ru>
2023-09-13 21:45:41 +0300

    page 3

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,15 @@
 $(function () {
 
 })
+
+window.addEventListener('DOMContentLoaded', () => {
+
+const menu = document.querySelector('.mobile-menu');
+const mobile = document.querySelector('.nav-icon');
+
+mobile.addEventListener('click', function() {
+    this.classList.toggle('nav-icon--active')
+    menu.classList.toggle('nav--active');
+})
+
+})
